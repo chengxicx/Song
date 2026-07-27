deb2afeb095a8e617dbcfdbfecb09a0b0df2baf7
chengxicn <chengxicn@users.noreply.github.com>
2026-07-28 02:33:24 +0800

    Fix tag and new word filters: remove title override, add filtNewWord to backend

--- lute/book/routes.py
+++ lute/book/routes.py
@@ -39,6 +39,7 @@ def _load_term_custom_filters(request_form, parameters):
     filter_param_names = [
         "filtLanguage",
         "filtTag",
+        "filtNewWord",
     ]
     request_params = request_form.to_dict(flat=True)
     for p in filter_param_names:
